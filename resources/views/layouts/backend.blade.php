f35ab84b29c6e4d278a30c9c8108b97f158b319c
Verrya <evan_lefevre@orange.fr>
2023-01-08 14:54:14 +0100

    Update Navbars
    Improve language menu
    Add Products + Purchases button on login screen

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -11,6 +11,7 @@
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        {{-- Left Side --}}
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,6 +31,56 @@
                 </ul>
             </div>
         </div>
+        {{-- Right Side --}}
+        <ul class="navbar-nav ms-auto">
+
+            @guest
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ __('Language') }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('lang', 'fr') }}">Français</a>
+                        <a class="dropdown-item" href="{{ route('lang', 'en') }}">English</a>
+                    </div>
+                <li class="nav-item">
+
+                    <!-- Authentication Links -->
+                </li>
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
     </nav>
 
     <main>
